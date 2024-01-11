f9e95de98e5ef3bfb1071222643879496735f122
zyxkad <zyxkad@gmail.com>
2024-01-11 09:01:36 -0700

    fix some format

--- api/v0/analyze.go
+++ api/v0/analyze.go
@@ -1,17 +1,16 @@
-
 // Serverless function web API
 // Copyright (C) 2023  zyxkad@gmail.com
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package handler
@@ -25,20 +24,20 @@ import (
 	"strconv"
 
 	"github.com/GlobeMC/mcla"
-	msldb "github.com/kmcsr/mcla-serverless/errdb"
+	"github.com/kmcsr/mcla-serverless/errdb"
 )
 
 type Map = map[string]any
 
-func writeError(w http.ResponseWriter, status int, err string){
+func writeError(w http.ResponseWriter, status int, err string) {
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(Map{
 		"status": "error",
-		"error": err,
+		"error":  err,
 	})
 }
 
-func Handler(w http.ResponseWriter, r *http.Request){
+func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET", "POST":
 	default:
@@ -68,12 +67,12 @@ func Handler(w http.ResponseWriter, r *http.Request){
 
 	if mediatype, _, err := mime.ParseMediaType(r.Header.Get("Content-Type")); err != nil {
 		writeError(w, http.StatusBadRequest, fmt.Sprintf("Error when parsing Content-Type: %v", err))
-	}else if mediatype == "multipart/form-data" {
+	} else if mediatype == "multipart/form-data" {
 		writeError(w, http.StatusUnsupportedMediaType, "multifile doesn't supported yet")
 		return
-	}else if mediatype == "text/plain" || mediatype == "application/octet-stream" {
-	}else if mediatype == "application/x-www-form-urlencoded" {
-	}else{
+	} else if mediatype == "text/plain" || mediatype == "application/octet-stream" {
+	} else if mediatype == "application/x-www-form-urlencoded" {
+	} else {
 		writeError(w, http.StatusUnsupportedMediaType, fmt.Sprintf("Unsupport content type %q", mediatype))
 		return
 	}
@@ -87,7 +86,7 @@ LOOP_RES:
 			if res == nil { // done
 				break LOOP_RES
 			}
-			res.Matched = filtered(res.Matched, func(m mcla.SolutionPossibility)(bool){
+			res.Matched = filtered(res.Matched, func(m mcla.SolutionPossibility) bool {
 				return m.Match >= matchRate
 			})
 			ress = append(ress, res)
@@ -100,16 +99,16 @@ LOOP_RES:
 
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(Map{
-		"status": "ok",
-		"_db_version": msldb.DefaultErrDB.GetVersion(),
-		"res": ress,
+		"status":      "ok",
+		"_db_version": errdb.DefaultErrDB.GetVersion(),
+		"res":         ress,
 	}); err != nil {
 		fmt.Println("Error when encoding to ResponseWriter:", err)
 	}
 }
 
-func filtered[T comparable](arr []T, cb func(T)(bool))(res []T){
-	res = make([]T, 0, len(arr) / 2)
+func filtered[T comparable](arr []T, cb func(T) bool) (res []T) {
+	res = make([]T, 0, len(arr)/2)
 	for _, v := range arr {
 		if cb(v) {
 			res = append(res, v)
@@ -118,17 +117,16 @@ func filtered[T comparable](arr []T, cb func(T)(bool))(res []T){
 	return
 }
 
-
 type ErrorResult struct {
 	Error   *mcla.JavaError            `json:"error"`
 	Matched []mcla.SolutionPossibility `json:"matched"`
 	File    string                     `json:"file"`
 }
 
-func analyzeLogErrors(r io.Reader)(<-chan *ErrorResult, <-chan error){
+func analyzeLogErrors(r io.Reader) (<-chan *ErrorResult, <-chan error) {
 	resCh := make(chan *ErrorResult, 3)
 	errCh := make(chan error, 0)
-	go func(){
+	go func() {
 		defer close(resCh)
 		jerrCh, errC := mcla.ScanJavaErrorsIntoChan(r)
 	LOOP:
@@ -157,5 +155,5 @@ func analyzeLogErrors(r io.Reader)(<-chan *ErrorResult, <-chan error){
 }
 
 var defaultAnalyzer = &mcla.Analyzer{
-	DB: msldb.DefaultErrDB,
+	DB: errdb.DefaultErrDB,
 }
